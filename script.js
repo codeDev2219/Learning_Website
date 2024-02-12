917e1d39d5bb34c5e8dcad18dd6f77068037ca7e
Vikas Sharma <126887064+codeDev2219@users.noreply.github.com>
2024-02-12 21:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var crsr = document.querySelector("#cursor")
-var crsrblur = document.querySelector("#cursor-blur")
+let crsr = document.querySelector("#cursor")
+let crsrblur = document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove", function (dets) {
     crsr.style.left = dets.x + "px"
@@ -16,7 +16,7 @@ gsap.to("#nav", {
     scrollTrigger: {        //we are triggering #nav and scrolling body
         trigger: "#nav",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top -10%",
         end: "top -11%",
         scrub: 1
